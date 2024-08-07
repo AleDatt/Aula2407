eaf410783d645c4e6e6ce82cd2e2e9a8c411b452
Alexandre Dattilio <aledattilio@hotmail.com>
2024-08-07 14:45:47 -0300

    subindo

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,35 @@
+﻿using Aula2407.Data;
+using Aula2407.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula2407.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly AulaContext _context;
+
+        public ClienteController(AulaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CadastroCliente([Bind("Id,Nome,RG,CPF,Usuario,Senha,CEP,UF,Cidade,Bairro,Rua,Numero,Complemento")] Cliente cliente)
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "NossoApp");
+            }
+            return View(cliente);
+        }
+    }
+}
+
---
+++ Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula2407.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public string Marca { get; set; }
+        public string Tamanho { get; set; }
+        public string DataValidade { get; set;}
+        public decimal ValorUnitario { get; set;}
+    }
+}
+       
